7947119ffe3f35a5c46c6dd133d8b2864ab7c531
DaxVeramendi <dax.veramendi@gmail.com>
2023-07-13 12:28:19 -0500

    Eliminacion del primer Code Smell

--- src/RentalInfo.java
+++ src/RentalInfo.java
@@ -3,11 +3,14 @@ import java.util.HashMap;
 public class RentalInfo {
 
   public String statement(Customer customer) {
+    String reg = "regular";
+    String chi = "childrens";
+    String ne = "new";
     HashMap<String, Movie> movies = new HashMap();
-    movies.put("F001", new Movie("You've Got Mail", "regular"));
-    movies.put("F002", new Movie("Matrix", "regular"));
-    movies.put("F003", new Movie("Cars", "childrens"));
-    movies.put("F004", new Movie("Fast & Furious X", "new"));
+    movies.put("F001", new Movie("You've Got Mail", reg));
+    movies.put("F002", new Movie("Matrix", reg));
+    movies.put("F003", new Movie("Cars", chi));
+    movies.put("F004", new Movie("Fast & Furious X", ne));
 
     double totalAmount = 0;
     int frequentEnterPoints = 0;
@@ -16,16 +19,16 @@ public class RentalInfo {
       double thisAmount = 0;
 
       // determine amount for each movie
-      if (movies.get(r.getMovieId()).getCode().equals("regular")) {
+      if (movies.get(r.getMovieId()).getCode().equals(reg)) {
         thisAmount = 2;
         if (r.getDays() > 2) {
           thisAmount = ((r.getDays() - 2) * 1.5) + thisAmount;
         }
       }
-      if (movies.get(r.getMovieId()).getCode().equals("new")) {
+      if (movies.get(r.getMovieId()).getCode().equals(ne)) {
         thisAmount = r.getDays() * 3;
       }
-      if (movies.get(r.getMovieId()).getCode().equals("childrens")) {
+      if (movies.get(r.getMovieId()).getCode().equals(chi)) {
         thisAmount = 1.5;
         if (r.getDays() > 3) {
           thisAmount = ((r.getDays() - 3) * 1.5) + thisAmount;
@@ -35,7 +38,7 @@ public class RentalInfo {
       //add frequent bonus points
       frequentEnterPoints++;
       // add bonus for a two day new release rental
-      if (movies.get(r.getMovieId()).getCode() == "new" && r.getDays() > 2) frequentEnterPoints++;
+      if (movies.get(r.getMovieId()).getCode().equals(ne) && r.getDays() > 2) frequentEnterPoints++;
 
       //print figures for this rental
       result += "\t" + movies.get(r.getMovieId()).getTitle() + "\t" + thisAmount + "\n";
